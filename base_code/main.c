20dc746ae0591abba8c454e1f2140cf0a0a0f8f3
Yoomin114 <goldchu@sookmyung.ac.kr>
2024-12-20 22:51:31 +0900

    Modifying main.c
    Modified to import the functions created in each c file in main.c so that they can function normally.

--- base_code/main.c
+++ base_code/main.c
@@ -24,11 +24,12 @@ int main() {
     HealthData health_data = {0};
     
     // Tocode: to read the list of the exercises and diets
-    
+    loadExercises(EXERCISEFILEPATH); // Load exercises from file
+    loadDiets(DIETFILEPATH);         // Load diets from file
 
     // ToCode: to run the "Healthcare Management Systems" until all calories are used up or the user wants to exit the system
     do {
-    	if ( ){
+    	if (health_data.total_calories_intake >= DAILY_CALORIE_GOAL){
             printf("You have consumed all your calories for today! \n");
 		} 
 		else{
@@ -46,28 +47,29 @@ int main() {
 		// ToCode: to run the sysmtem based on the user's choice
         switch (choice) {
             case 1:
-            	
+            	inputExercise(&health_data); // Call function to input exercise data
                 break;
                 
             case 2:
-            	
+            	inputDiet(&health_data);     // Call function to input diet data
                 break;
                 
             case 3:
-            	
+            	printHealthData(&health_data); // Display logged exercise and diet information
                 break;
                 
             case 4:
             	
-    			printf("Exit the system.\n");
+    			printf("Exit the system.\n"); // Exit message
     			printf("=======================================================================\n");
                 break;
                 
             default:
-                printf("[Error] Invalid option. \n");
+                printf("[Error] Invalid option. \n"); // Error message for invalid option
                 printf("Please try again! \n");
         }
-    } while ( );
+    } while (health_data.total_calories_intake < DAILY_CALORIE_GOAL && choice != 4); 
+	// Continue until calories are consumed or user exits
 
     return 0;
 }
